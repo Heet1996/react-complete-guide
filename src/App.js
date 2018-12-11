34b20541bbbb725ceb012fbe44e15edece2520de
Heet1996 <heet1885@gmail.com>
2018-12-11 23:06:40 +0530

    state and props with binding

--- src/App.js
+++ src/App.js
@@ -3,11 +3,47 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state={
+    persons:[
+      {
+        name:'Heet',
+        age:20
+      },
+      {
+        name:'John',
+        age:22
+      },
+      {
+        name:'Max',
+        age:23
+      }
+    ]
+  }
+  switchNameHandler=(newName)=>{
+    this.setState({
+      persons:[
+        {name:newName,age:20},
+        {name:'joey',age:22},
+        {name:'Max', age:23}
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi,there </h1>
-        <Person />
+        <button onClick={()=>this.switchNameHandler('rachel')}>Switch Name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age} 
+        click={this.switchNameHandler.bind(this,'Ross')}
+        />
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}>My Hobbies are:Running</Person>
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age} />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,10 @@
 import React from 'react';
-const Person=()=>{
+const Person=(props)=>{
     return(
-        <p>I'm a person</p>
+        <div>
+        <p onClick={props.click}>I'm {props.name} and I'm {props.age} year old!</p>
+        <p>{props.children}</p>
+        </div>
     );
 }
 export default Person;
